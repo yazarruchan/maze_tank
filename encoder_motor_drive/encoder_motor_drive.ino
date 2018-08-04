ba58f1b41bedc1f6ab72a58d565c37d70d37fbff
Ruçhan Yazar <yazarruchan@gmail.com>
2018-08-04 18:57:56 +0300

    Pinler ve kod düzenlemesi
    -Encoder pinleri düzgün çalışacak şekilde düzenlendi.  Artık iki encoderde uyumlu ve düzgün çalışıyor.
    
    -Bazı fonksiyonlarda döngüler daha iyi çalışacak şekilde düzenlendi. While yerine for döngüleri kullanıldı.
    
    -Loopta sürekli döngüde çalışmama sorunu giderildi. 
    
    -Geri sayım 5 Saniyeden 3 saniyeye indirildi.

--- encoder_motor_drive/encoder_motor_drive.ino
+++ encoder_motor_drive/encoder_motor_drive.ino
@@ -1,7 +1,7 @@
 #include <Encoder.h>
 
-Encoder enc(2,3);
-Encoder enc2(4,5);
+Encoder enc(2,4);
+Encoder enc2(3,5);
 const int in1 = 6;     // Motor sürücümüze bağladığımız pinleri tanımlıyoruz
 const int in2 = 7;
 const int in3 = 8;
@@ -13,7 +13,7 @@ const int in4 = 9;
 int echoPinOn = 10;*/
 int hiz =10;
 int hiz2 =11;
-
+int deneme=1;
 long newPosition, newPosition2;
 /*long sureSol;
 long sureSag;
@@ -42,15 +42,17 @@ void setup() {
 
 void loop() {
   
-  Serial.println("Calismasina son");
-  for(int sayac; sayac<5; sayac++){
-  Serial.print(5-sayac);
-  Serial.println(" saniye.");
+  //Serial.println("Calismasina son");
+  for(int sayac; sayac<3; sayac++){
+  //Serial.print(3-sayac);
+  //Serial.println(" saniye.");
   delay(1000);
   }
-  
-  ileri_x_kare(100);
-  
+ // encoderDegerYazdir();
+  Serial.print(deneme);
+  Serial.println(". deneme");
+  ileri_x_kare(1);
+  deneme++;
  
  /* Uzaklık Sensörü Bölümü
   digitalWrite(trigPin, LOW);  // sensör pasif hale getirildi 
@@ -105,10 +107,38 @@ void bir_kare_ileri(){
 }
 
 void ileri_x_kare(int kare_sayisi){
+  
   analogWrite(hiz,  150);
   analogWrite(hiz2, 150);
  for(int i=0; i<kare_sayisi;i++){
-    while(newPosition < (i+1)*950 || newPosition2 < (i+1)*950){
+    for(newPosition = enc.read();newPosition < (i+1)*740;newPosition = enc.read()){
+      if (newPosition != oldPosition) {
+        oldPosition = newPosition;
+       // encoderDegerYazdir();
+        }
+      digitalWrite(in1, LOW); // Duruma göre değiştirelecek ileri veya geri olamsına göre.
+      digitalWrite(in2, HIGH);
+      digitalWrite(in3, LOW); // in3, in4 ayaranacak.
+      digitalWrite(in4, HIGH);
+
+    }
+  
+  }
+ motorDurdur();
+ delay(1000);
+ encoderDegerYazdir();
+ 
+ encoderSifirla();
+ delay(750);
+//Serial.println("Encoder Sifirlandi.");
+ //encoderDegerYazdir();
+}
+
+void ileri_x_kare_ikili(int kare_sayisi){
+  analogWrite(hiz,  150);
+  analogWrite(hiz2, 150);
+ for(int i=0; i<kare_sayisi;i++){
+    while(newPosition < (i+1)*500 || newPosition2 < (i+1)*950){
       newPosition = enc.read();
       if (newPosition != oldPosition || newPosition2 != oldPosition2) {
         oldPosition = newPosition;
@@ -154,7 +184,7 @@ void ileri_x_mm(){// yapım aşamasında
       digitalWrite(in3, LOW); // in3, in4 ayaranacak.
       digitalWrite(in4, HIGH);
     }
-  motorDurdur();
+   motorDurdur();
    encoderSifirla();
 }
 
@@ -203,18 +233,22 @@ void encoderDegerYazdir(){
     Serial.print(enc.read()); //ENKODERDEN GELEN BİLGİYİ GÖRÜNTÜLEMEK İÇİN
     Serial.print(" --- Encoder 2 = ");
     Serial.println(enc2.read());
+   
 }
 
 void encoderSifirla(){
     delay(750);
     enc.write(0);
     enc2.write(0);
+    oldPosition  = -999;
+    oldPosition2 = -999;
+    
 }
 
 void motorDurdur(){
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, HIGH);
 }
 
